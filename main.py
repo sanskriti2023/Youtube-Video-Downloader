fcf35c5a689a3fd1b2371c7fdeb52ced72e7b6e8
sanskriti2023 <sanskriti09singh@gmail.com>
2022-01-04 21:53:10 +0530

    added more video details

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from pytube import YouTube
 url = input("Enter YouTube Link: ")
 videoUrl = YouTube(url)
 
+print("Video Title: ", videoUrl.title) #video title
+print("Video Length: ", videoUrl.length) #video length
+print("No. of Views: ", videoUrl.views)  #video view
+print("Thumbnail of the Video ", videoUrl.thumbnail_url) #video thumbnail
+
 videoDownload = videoUrl.streams.get_highest_resolution()
 
 #video downloading

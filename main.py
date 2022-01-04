24620b5c1d163e02ec800bb35c238fac9f7698da
hariom09090 <hariom24568@gmail.com>
2022-01-04 12:45:24 +0530

    adding comments

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+#importing library
 from pytube import YouTube
 
+#get the url of the video 
 url = input("Enter YouTube Link: ")
 videoUrl = YouTube(url)
 
 videoDownload = videoUrl.streams.get_highest_resolution()
 
+#video downloading
 print("Downloading the Video: ")
 videoDownload.download()
 print("Video Downloaded")
